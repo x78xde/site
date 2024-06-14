21f28b03dc2e8e3740cea5d2dcfac6a8caae0b5d
x78xde <133084801+x78xde@users.noreply.github.com>
2024-06-13 20:34:52 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loginBox = document.querySelector(".login-box");
+    if (loginBox) {
+        loginBox.classList.add("slide-down");
+    }
+});
